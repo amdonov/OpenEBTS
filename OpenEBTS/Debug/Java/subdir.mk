d74043c7c314a77018106879b86116ae2cb0cbe7
Aaron Donovan <amdonov@gmail.com>
2013-03-15 09:07:20 -0400

    added the -fPIC argument to gcc as instructed by linker

--- OpenEBTS/Debug/Java/subdir.mk
+++ OpenEBTS/Debug/Java/subdir.mk
@@ -20,7 +20,7 @@ C_DEPS += \
 Java/%.o: ../Java/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -DUNICODE -D_DEBUG -I../. -I../.. -I../Java -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	gcc -fPIC -DUNICODE -D_DEBUG -I../. -I../.. -I../Java -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- OpenEBTS/ReleaseA/Java/subdir.mk
+++ OpenEBTS/ReleaseA/Java/subdir.mk
@@ -20,7 +20,7 @@ C_DEPS += \
 Java/%.o: ../Java/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I../. -I../.. -I../Java -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	gcc -fPIC -I../. -I../.. -I../Java -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
